c6b0a8b5075d5dc2a7920b99a5ab4f285a3715ec
JoelOna <onaferrerjoel@gmail.com>
2024-10-10 20:07:41 +0200

    docker, y comentarios

--- resources/js/components/CommentButton.vue
+++ resources/js/components/CommentButton.vue
@@ -1,91 +1,130 @@
-<template> 
-      <div :data-modal-target="'modal-' + props.post_id" :data-modal-toggle="'modal-' + props.post_id" class="p-4 flex flex-wrap gap-1">
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-message-circle">
-          <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-          <path d="M3 20l1.3 -3.9c-2.324 -3.437 -1.426 -7.872 2.1 -10.374c3.526 -2.501 8.59 -2.296 11.845 .48c3.255 2.777 3.695 7.266 1.029 10.501c-2.666 3.235 -7.615 4.215 -11.574 2.293l-4.7 1" />
-        </svg>
-        {{ totalComments }}
-      </div>
-    
-      <!-- Modal -->
-      <div :id="'modal-' + props.post_id" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
-        <div class="relative p-4 w-full max-w-4xl max-h-full">
-          <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
-            <!-- Modal header -->
-            <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
-                <div class="text-red-700">
-                    {{ errorMessage }}
-                 </div>
-              <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" :data-modal-hide="'modal-' + props.post_id">
-                <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
-                  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
-                </svg>
-                <span class="sr-only">Close modal</span>
+<template>
+  <div :data-modal-target="'modal-' + props.post_id" :data-modal-toggle="'modal-' + props.post_id"
+    class="p-4 flex flex-wrap gap-1">
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor"
+      stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+      class="icon icon-tabler icons-tabler-outline icon-tabler-message-circle">
+      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+      <path
+        d="M3 20l1.3 -3.9c-2.324 -3.437 -1.426 -7.872 2.1 -10.374c3.526 -2.501 8.59 -2.296 11.845 .48c3.255 2.777 3.695 7.266 1.029 10.501c-2.666 3.235 -7.615 4.215 -11.574 2.293l-4.7 1" />
+    </svg>
+    {{ totalComments }}
+  </div>
+
+  <!-- Modal -->
+  <div :id="'modal-' + props.post_id" tabindex="-1" aria-hidden="true"
+    class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+    <div class="relative p-4 w-full max-w-4xl lg:max-w-6xl max-h-full">
+      <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+        <!-- Modal header -->
+        <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
+          <div class="text-red-700">
+            {{ errorMessage }}
+          </div>
+          <button type="button"
+            class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+            :data-modal-hide="'modal-' + props.post_id">
+            <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+            </svg>
+            <span class="sr-only">Close modal</span>
+          </button>
+        </div>
+        <!-- Modal body -->
+        <div class="transition-all hover:transition-all m-4">
+
+          <div class="w-full mb-4 border border-gray-200 rounded-lg bg-gray-50 dark:bg-gray-700 dark:border-gray-600">
+            <div class="px-4 py-2 bg-white rounded-t-lg dark:bg-gray-800">
+              <textarea rows="4"
+                class="w-full px-0 text-sm text-gray-900 bg-white border-0 dark:bg-gray-800 focus:ring-0 dark:text-white dark:placeholder-gray-400"
+                placeholder="Write a comment..." required v-model="comment"></textarea>
+            </div>
+            <div class="flex items-center justify-between px-3 py-2 border-t dark:border-gray-600">
+              <button @click="submmitComment"
+                class="inline-flex items-center py-2.5 px-4 text-xs font-medium text-center text-white bg-blue-700 rounded-lg focus:ring-4 focus:ring-blue-200 dark:focus:ring-blue-900 hover:bg-blue-800">
+                Post comment
               </button>
-            </div>
-            <!-- Modal body -->
-            <div class="transition-all hover:transition-all m-4">
-              <text-area :name="'comment'" v-model="comment"></text-area>
-              <post-comments :post_id="props.post_id" :action="updateAction"></post-comments>
-            </div>
-            <!-- Modal footer -->
-            <div class="flex items-center p-4 md:p-5 border-t border-gray-200 rounded-b dark:border-gray-600">
-              <button @click="submmitComment" type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+              <!-- <div class="flex ps-0 space-x-1 rtl:space-x-reverse sm:ps-2">
+              <button type="button" class="inline-flex justify-center items-center p-2 text-gray-500 rounded cursor-pointer hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600">
+                  <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 12 20">
+                       <path stroke="currentColor" stroke-linejoin="round" stroke-width="2" d="M1 6v8a5 5 0 1 0 10 0V4.5a3.5 3.5 0 1 0-7 0V13a2 2 0 0 0 4 0V6"/>
+                   </svg>
+                  <span class="sr-only">Attach file</span>
+              </button>
+              <button type="button" class="inline-flex justify-center items-center p-2 text-gray-500 rounded cursor-pointer hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600">
+                  <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 20">
+                       <path d="M8 0a7.992 7.992 0 0 0-6.583 12.535 1 1 0 0 0 .12.183l.12.146c.112.145.227.285.326.4l5.245 6.374a1 1 0 0 0 1.545-.003l5.092-6.205c.206-.222.4-.455.578-.7l.127-.155a.934.934 0 0 0 .122-.192A8.001 8.001 0 0 0 8 0Zm0 11a3 3 0 1 1 0-6 3 3 0 0 1 0 6Z"/>
+                   </svg>
+                  <span class="sr-only">Set location</span>
+              </button>
+              <button type="button" class="inline-flex justify-center items-center p-2 text-gray-500 rounded cursor-pointer hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600">
+                  <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 18">
+                       <path d="M18 0H2a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2Zm-5.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm4.376 10.481A1 1 0 0 1 16 15H4a1 1 0 0 1-.895-1.447l3.5-7A1 1 0 0 1 7.468 6a.965.965 0 0 1 .9.5l2.775 4.757 1.546-1.887a1 1 0 0 1 1.618.1l2.541 4a1 1 0 0 1 .028 1.011Z"/>
+                   </svg>
+                  <span class="sr-only">Upload image</span>
+              </button>
+          </div> -->
             </div>
           </div>
+          <!-- <text-area :name="'comment'" v-model="comment"></text-area> -->
+          <post-comments :post_id="props.post_id" :action="updateAction"></post-comments>
+        </div>
+        <!-- Modal footer -->
+        <div class="flex items-center p-4 md:p-5 border-t border-gray-200 rounded-b dark:border-gray-600">
         </div>
       </div>
+    </div>
+  </div>
 
 </template>
 
 <script setup>
-    import {ref,onMounted} from 'vue'
-    import axios from 'axios'
+import { ref, onMounted } from 'vue'
+import axios from 'axios'
 
-    const comment = ref('What are you thinking about?');
-    const props = defineProps({
-        user_id: Number,
-        post_id: Number,
-    })
+const comment = ref('test componentee ref');
+const props = defineProps({
+  user_id: Number,
+  post_id: Number,
+})
 
-    const updateAction = ref(false)
-    const errorMessage = ref('')
-    const submmitComment = async () => {
-        try {
-                const response = await axios.post('/comment',{
-                    user_id: props.user_id,
-                    post_id: props.post_id,
-                    comment: comment.value
-                }); 
-                if (response.status === 200) {
-                    totalCommentsRequest()
-                }
-                console.log(response.message);
-                
-                    updateAction.value = !updateAction.value
-            } catch (error) {
-                errorMessage.value = error.response.data.message
-            }
+const updateAction = ref(false)
+const errorMessage = ref('')
+const submmitComment = async () => {
+  try {
+    const response = await axios.post('/comment', {
+      user_id: props.user_id,
+      post_id: props.post_id,
+      comment: comment.value
+    });
+    if (response.status === 200) {
+      totalCommentsRequest()
     }
-    const totalComments = ref(0)
+    console.log(comment.value);
 
-    const totalCommentsRequest = async ()=> {
-        try{
-            const response  = await axios.get(`/comments/${props.post_id}/total`)
-            if (response.status === 200) {
-                totalComments.value = response.data
-            }
-        }catch(error){
+    updateAction.value = !updateAction.value
+  } catch (error) {
+    errorMessage.value = error.response.data.message
+  }
+}
+const totalComments = ref(0)
 
-        }
+const totalCommentsRequest = async () => {
+  try {
+    const response = await axios.get(`/comments/${props.post_id}/total`)
+    if (response.status === 200) {
+      totalComments.value = response.data
     }
+  } catch (error) {
 
-    onMounted(() => {
-        totalCommentsRequest()
-    })
+  }
+}
+
+onMounted(() => {
+  totalCommentsRequest()
+})
 
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -76,6 +76,11 @@
                 {{ __('Dashboard') }}
             </x-responsive-nav-link>
         </div>
+        <div class="pt-2 pb-3 space-y-1">
+            <x-responsive-nav-link :href="route('profile.update')" :active="request()->routeIs('profile.update')">
+                {{ __('Profile') }}
+            </x-responsive-nav-link>
+        </div>
 
         <!-- Responsive Settings Options -->
         <div class="pt-4 pb-1 border-t border-gray-200 dark:border-gray-600">
